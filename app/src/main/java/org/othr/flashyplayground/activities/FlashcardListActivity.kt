810d009872b037cf45d28d1b96b935e599982924
AlexW850 <alexander.wastl@st.oth-regensburg.de>
2021-12-11 11:44:30 +0100

    Added basic support for RecyclerView and corresponding row layout

---
+++ app/src/main/java/org/othr/flashyplayground/activities/FlashcardListActivity.kt
@@ -0,0 +1,12 @@
+package org.othr.flashyplayground.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.othr.flashyplayground.R
+
+class FlashcardListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flashcard_list)
+    }
+}
